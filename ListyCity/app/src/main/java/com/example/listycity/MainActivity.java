0ff9884e2d62a6e86c084f135a875f0f1e6fb61a
Leo <qq2910894473@gmail.com>
2026-01-16 14:41:07 -0700

    final i guess

--- ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
+++ ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.listycity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -13,12 +15,19 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import android.widget.EditText;
+import androidx.appcompat.app.AlertDialog;
+
+
 public class MainActivity extends AppCompatActivity {
 
     ListView cityList;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
 
+    Button addButton;
+    Button deleteButton;
+    int selectedPosition = -1;   // -1 = nothing selected
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
         cityList = findViewById(R.id.city_list);
+        addButton = findViewById(R.id.add_city_button);
+        deleteButton = findViewById(R.id.delete_city_button);
+
 
         String []cities = {"Edmonton","Middle of no where"};
 
@@ -41,7 +53,31 @@ public class MainActivity extends AppCompatActivity {
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
         cityList.setAdapter(cityAdapter);
 
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            selectedPosition = position;
+        });
 
+        addButton.setOnClickListener(v -> {
+            EditText input = new EditText(this);
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Enter city name")
+                    .setView(input)
+                    .setPositiveButton("CONFIRM", (dialog, which) -> {
+                        String cityName = input.getText().toString();
+                        dataList.add(cityName);
+                        cityAdapter.notifyDataSetChanged();
+                    })
+                    .show();
+        });
+
+        deleteButton.setOnClickListener(v -> {
+            if (selectedPosition >= 0 && selectedPosition < dataList.size()) {
+                dataList.remove(selectedPosition);
+                cityAdapter.notifyDataSetChanged();
+                selectedPosition = -1;
+            }
+        });
 
     }
 }
